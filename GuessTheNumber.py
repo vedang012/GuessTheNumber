19da54553609ef02fa80c56e1269070842c11595
Vedang Solaskar <68228783+VedangSolaskar@users.noreply.github.com>
2021-05-18 18:03:35 +0530

    Update GuessTheNumber.py

--- GuessTheNumber.py
+++ GuessTheNumber.py
@@ -9,23 +9,26 @@ You have 9 chances.
 If you will be unable to guess that number in 9 chances the game will be overed. 
 Best Luck to you
 ''')
-while Guesses != 0:
-    print("Chances left : " + str(Guesses))
-    Guess = int(input())
-    if Guess == number:
-        print("You Won, Congratulation!")
-        break
-    elif Guess > number:
-        print("Number is smaller than your input")
-        Guesses = Guesses - 1
-    elif Guess < number:
-        print("Number is greater than your input")
-        Guesses = Guesses - 1
-    else:
-        print("You Won, Congratulation!")
-        break
-
+try:
+    while Guesses != 0:
+        print("Chances left : " + str(Guesses))
+        Guess = int(input())
+        if Guess == number:
+            print("You Won, Congratulation!")
+            break
+        elif Guess > number:
+            print("Number is smaller than your input")
+            Guesses = Guesses - 1
+        elif Guess < number:
+            print("Number is greater than your input")
+            Guesses = Guesses - 1
+        else:
+            print("You Won, Congratulation!")
+            break
+except Exception as e:
+    print(e)
+    print("You have to enter number not String")
 if Guesses == 0:
     print("You don't have enough guesses left")
-    print("the number was " + number)
+    print("the number was " + str(number))
     print("Game Over")
